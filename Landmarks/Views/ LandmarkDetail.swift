5a28086357a6ef8adebce9b8ab8484b2208746a2
Faisal Manzer <faisal_manzer@yahoo.in>
2020-12-13 15:24:44 +0530

    Added basic App

---
+++ Landmarks/Views/ LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//   LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Faisal Manzer on 13/12/20.
+//
+
+import SwiftUI
+
+struct _LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        _LandmarkDetail()
+    }
+}
